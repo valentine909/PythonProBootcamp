43b0b82932da7e4f6fc5bc72dc946f1c52a6c48a
valentine909 <valentine.nazarov@gmail.com>
2021-04-29 16:48:46 +0300

    Day 1 commit

---
+++ Day 1. Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello in our Band Name Generator")
+city = input("What's the name of a city you have born?: ")
+pet = input("What your favorite animal?: ")
+print("Your band name should be: ", f'{city} {pet} band')
