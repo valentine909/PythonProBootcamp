219c7b06f2615828915d2fc6779adcff5483904c
valentine909 <valentine.nazarov@gmail.com>
2021-04-29 16:52:15 +0300

    Day 1 commit typos corrected

--- Day 1. Band Name Generator.py
+++ Day 1. Band Name Generator.py
@@ -1,4 +1,4 @@
-print("Hello in our Band Name Generator")
-city = input("What's the name of a city you have born?: ")
-pet = input("What your favorite animal?: ")
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you have born?: ")
+pet = input("What's your favorite animal?: ")
 print("Your band name should be: ", f'{city} {pet} band')
