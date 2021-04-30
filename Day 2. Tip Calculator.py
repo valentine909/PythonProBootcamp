1cea88c5751302a6ccef1e0ffc7cce50b5f4acbb
valentine909 <valentine.nazarov@gmail.com>
2021-04-30 09:45:11 +0300

    Day 2 commit tip calculator released

---
+++ Day 2. Tip Calculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill?: "))
+percent = float(input("What percentage tip would you like to give? 10, 12, or 15?: "))
+people = float(input("How many people to split the bill?: "))
+payment = total_bill * (100 + percent) / 100 / people
+print(f"Each person should pay: ${payment}")
