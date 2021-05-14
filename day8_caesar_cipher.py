157b2045f6c7e884647d6ad99ca5c3556431e0fd
valentine909 <valentine.nazarov@gmail.com>
2021-05-14 09:35:20 +0300

    commit day 8 caesar cipher added

--- day8_caesar_cipher.py
+++ day8_caesar_cipher.py
@@ -1,3 +1,6 @@
+from day8_art import logo
+
+
 def take_input():
     mode = input("Type 'encode' to encrypt, type 'decode' to decode:\n")
     message = input("Type your message:\n")
@@ -8,19 +11,25 @@ def take_input():
 def encode(message, shift):
     new_message = ''
     for i in message:
-        new_position = (ord(i) + shift) - (ord(i) + shift) // (97 + 25) * 25
-        new_message += chr(new_position)
+        if i.isalpha():
+            new_position = (ord(i) + shift) - (ord(i) + shift) // (97 + 25) * 25
+            new_message += chr(new_position)
+        else:
+            new_message += i
     return new_message
 
 
 def decode(message, shift):
     new_message = ''
     for i in message:
-        if ord(i) - shift < 97:
-            new_position = (ord(i) - shift) + 25
+        if i.isalpha():
+            if ord(i) - shift < 97:
+                new_position = (ord(i) - shift) + 25
+            else:
+                new_position = (ord(i) - shift)
+            new_message += chr(new_position)
         else:
-            new_position = (ord(i) - shift)
-        new_message += chr(new_position)
+            new_message += i
     return new_message
 
 
@@ -29,10 +38,11 @@ def cryptographer():
     if mode == 'encode':
         print(encode(message, shift))
     elif mode == 'decode':
-        print(encode(message, shift))
+        print(decode(message, shift))
     else:
         print("Clarify your input")
 
 
 if __name__ == '__main__':
+    print(logo)
     cryptographer()
