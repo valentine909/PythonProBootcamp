1dbfc1e30d500cabf71fdc8a8305010ed39589e4
valentine909 <valentine.nazarov@gmail.com>
2021-06-02 11:57:27 +0300

    refactored

--- day12_number_guessing_game.py
+++ day12_number_guessing_game.py
@@ -1,30 +1,40 @@
 import random
 from day12_art import logo
+EASY_LEVEL = 10
+HARD_LEVEL = 5
 
-print(logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-number = random.randrange(1, 101)
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-attempts = 0
-if difficulty == 'easy':
-    attempts = 10
-elif difficulty == 'hard':
-    attempts = 5
-else:
-    print("Wrong input!")
-    exit()
-while attempts:
-    print(f"You have {attempts} attempts remaining to guess the number")
-    guess = int(input("Make a guess: "))
-    attempts -= 1
-    if guess == number:
-        print("You win!")
-        print(f"The number was {number}")
-        exit()
-    elif guess > number:
-        print("Too high.\nGuess again.")
-    else:
-        print("Too low.\nGuess again.")
-print("You lose!")
-print(f"The number was {number}")
+
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    attempts = 0
+    if difficulty == 'easy':
+        attempts = EASY_LEVEL
+    elif difficulty == 'hard':
+        attempts = HARD_LEVEL
+    return attempts
+
+
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    number = random.randrange(1, 101)
+    turns = set_difficulty()
+    while turns:
+        print(f"You have {turns} attempts remaining to guess the number")
+        guess = int(input("Make a guess: "))
+        turns -= 1
+        if guess == number:
+            print("You win!")
+            print(f"The number was {number}")
+            return
+        elif guess > number:
+            print("Too high.")
+        else:
+            print("Too low.")
+    print("You lose!")
+    print(f"The number was {number}")
+
+
+if __name__ == '__main__':
+    game()
