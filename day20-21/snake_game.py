14b25a3daf7605bdd515c8549affa81217017509
valentine909 <valentine.nazarov@gmail.com>
2021-06-19 13:43:46 +0300

    day20/21 self-collision

--- day20-21/snake_game.py
+++ day20-21/snake_game.py
@@ -33,14 +33,25 @@ def enable_keys():
     screen.onkey(key='d', fun=snake.to_the_right)
 
 
+def is_food_collision():
+    return snake.head.distance(food) <= 15
+
+
 def is_wall_collision():
     x = WIDTH // 2 - 20
     y = HEIGHT // 2 - 20
     return snake.head.xcor() > x or snake.head.xcor() < -x or snake.head.ycor() > y or snake.head.ycor() < -y
 
 
+def is_self_collision():
+    for segment in snake[1:]:
+        if snake.head.distance(segment) < 10:
+            return True
+    return False
+
+
 def main():
-    global snake
+    global snake, food
     init_screen()
     screen.update()
     snake = Snake()
@@ -52,11 +63,11 @@ def main():
     while game_is_on:
         snake.move_forward()
         screen.update()
-        if snake.head.distance(food) <= 15:
+        if is_food_collision():
             food.move()
             snake.add_segment()
             score.refresh_score()
-        if is_wall_collision():
+        if is_wall_collision() or is_self_collision():
             game_is_on = False
             score.game_over()
         sleep(SNAKE_MOVE_DELAY)
