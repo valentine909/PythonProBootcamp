9be7adf97121bda002b2f8f6b0b32763533498d0
valentine909 <valentine.nazarov@gmail.com>
2021-06-19 12:34:36 +0300

    day20/21 food random position is aligned

--- day20-21/food.py
+++ day20-21/food.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-from random import randint
+from random import randrange
 from settings import SETTINGS
 
 WIDTH = SETTINGS['width']
@@ -11,13 +11,12 @@ class Food(Turtle):
         super(Food, self).__init__(shape='circle')
         self.color('blue')
         self.pu()
-        self.shapesize(0.5, 0.5)
         self.speed("fastest")
         self.move()
 
     def move(self):
         x_boundary = WIDTH // 2
         y_boundary = HEIGHT // 2
-        random_x = randint(-x_boundary + 20, x_boundary - 20)
-        random_y = randint(-y_boundary + 20, y_boundary - 20)
+        random_x = randrange(-x_boundary + 40, x_boundary - 40, 20)
+        random_y = randrange(-y_boundary + 40, y_boundary - 40, 20)
         self.goto(random_x, random_y)
