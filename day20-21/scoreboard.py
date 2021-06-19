48cbe23f7120cee72e7893451c81c349ed415f1d
valentine909 <valentine.nazarov@gmail.com>
2021-06-19 13:01:33 +0300

    day20/21 settings expanded

--- day20-21/scoreboard.py
+++ day20-21/scoreboard.py
@@ -2,6 +2,8 @@ from turtle import Turtle
 from settings import SETTINGS
 
 HEIGHT = SETTINGS['height']
+ALIGNMENT = 'center'
+FONT = ("Arial", 14, "bold")
 
 
 class Scoreboard(Turtle):
@@ -10,11 +12,10 @@ class Scoreboard(Turtle):
         self.color('white')
         self.score = -1
         self.pu()
-        self.goto(0, HEIGHT // 2 - 20)
-        self.pd()
+        self.goto(0, HEIGHT // 2 - 30)
         self.refresh_score()
 
     def refresh_score(self):
         self.clear()
         self.score += 1
-        self.write(f"Score: {self.score}", align='center', font=("Arial", 14, "bold"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
